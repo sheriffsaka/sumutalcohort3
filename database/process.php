c4e0dbdfdf8c80865aa245556e1886926dff9323
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2025-09-05 11:02:55 +0100

    Updated my process.php file

--- database/process.php
+++ database/process.php
@@ -14,9 +14,12 @@ $stmt = $conn->prepare("INSERT INTO contacts (fullname, email, telephone, messag
 $stmt->bind_param("ssss", $fname, $email, $tel, $message);
 
 if ($stmt->execute()) {
-    echo "New record created successfully";
+    echo "New record created successfully.<br>";
+    echo "Thank you $fname for the message: $message, we will get back to you @ $email.<br>";
 } else {
     echo "Error: " . $stmt->error;
+    echo "<br>";
+    echo "Something went wrong. Please try again!";
 }
 
 $stmt->close();
